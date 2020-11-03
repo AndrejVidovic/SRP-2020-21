bc37acf41e299ae27649beca7b821e6a9d46e884
AndrejVidovic <vidaftw@gmail.com>
2020-11-03 14:39:49 +0100

    Add initial login logic

--- postgres-express-node/services/index.js
+++ postgres-express-node/services/index.js
@@ -1,12 +1,15 @@
 const winston = require("winston");
+const LoginService = require("./login.service");
 const UserService = require("./user.service");
 const MedicalTestService = require("./medical-test.service");
 const { User, MedicalTest } = require("../models");
 
+
 const logger = winston.loggers.get("logger");
 
 exports.userServiceInstance = new UserService({ logger, userModel: User });
-exports.medicalTestServiceInstance = new MedicalTestService({
+exports.loginServiceInstance = new LoginService({ logger, userModel: User });
+exports.MedicalTestService=new MedicalTestService({
   logger,
-  testModel: MedicalTest,
+  testModel:MedicalTest,
 });
